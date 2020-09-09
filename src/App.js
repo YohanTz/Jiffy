23960a71919a14d4f04c7ac67ac3661b920ecf61
Yohan Tancrez <yohan.tancrez@epita.fr>
2020-09-09 16:45:07 +0200

    [ADD] Starting the project from scratch, finished background and logo, todo: rest of Header component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './Components/Header';
 
-function App() {
+const App = () => {
+  const [activeP, setActiveP] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header activeP={activeP} setActiveP={setActiveP} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/Header.css';
+
+const Header = ({ activeP, setActiveP }) => {
+  return (
+    <header>
+      <div className='logo'>Jiffy</div>
+    </header>
+  );
+};
+
+export default Header;
